cb4c8b1484676e28995cf2ba5a0715f4f410d08e
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2023-04-25 16:44:31 -0300

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById("butao");
+let texto = document.getElementById("texto");
+
+botao.addEventListener("click", function() {
+  texto.innerHTML = "Hello World!";
+
+  
+});
