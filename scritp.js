dbd8705a9a7e6b3ad55bea216f578942326926aa
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2023-04-25 18:59:25 -0300

    Add files via upload

--- scritp.js
+++ scritp.js
@@ -2,7 +2,9 @@ let botao = document.getElementById("butao");
 let texto = document.getElementById("texto");
 
 botao.addEventListener("click", function() {
-  texto.innerHTML = "Hello World!";
+  texto.innerHTML = "<p>Hello World!!</p>";
 
-  
+  console.log("Esse código foi feito por João Victor Rosinhole");
+
+  window.alert("O RM de meu criador é 97880");
 });
